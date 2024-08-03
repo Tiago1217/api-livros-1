46db58127c83ed372c6e3959cf0964351ab96005
Tiago1217 <tiagowwe17@hotmail.com>
2024-08-02 22:51:34 -0300

    novas funções

---
+++ app/Http/Controllers/TbllivrosController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TbllivrosController extends Controller
+{
+    //construir o crud.
+
+    //mostrar todos os registros da tabela livros
+    //crud-> Read(leitura) Select/Visualizar
+    public function index(){
+
+    }
+
+    //Mostrar um tipo de registro especifico
+    //crud-> Read(leitura) Select/Visualizar
+    public function show(){
+
+    }
+
+    //Cadastrar registros
+    ////crud-> Create (criar/cadastrar)
+    public function store(){
+
+    }
+    //alterar registros
+    //Crud -> update(alterar)
+    public function update(){
+
+    }
+
+    //deletar os registros
+    //crud -> delete(apagar)
+    public function destroy(){
+
+    }
+}
